9bf17d219fc2c344c7d2ca15bc91d2307ee756ef
ji <jiyongprk@gmail.com>
2022-04-09 14:47:39 -0400

    remove style out of jsx to css

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function ScoreBoard(props) {
     return (
         <div className='score-container text-container'>
-            <span Style="font-size: 1.5rem" className='score'>Score: <span className="score-number">{props.score}</span></span>
-            <span Style="font-size: 1.5rem" className='high-score'> High Score: <span className='hscore-number'>{props.highScore}</span></span>
+            <span className='score'>Score: <span className="score-number">{props.score}</span></span>
+            <span className='high-score'> High Score: <span className='hscore-number'>{props.highScore}</span></span>
         </div>
     )
 }
