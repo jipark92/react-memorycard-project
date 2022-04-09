bb8acc3e39fa3719c5860d5ce68e0cbd040286f2
ji <jiyongprk@gmail.com>
2022-04-08 22:53:48 -0400

    alertgame over when same card is touched twice

--- src/components/Content.js
+++ src/components/Content.js
@@ -23,33 +23,23 @@ export default function Content() {
     //check clicked
     const checkCard = (e) =>{
         animeDatas.find((animeData)=>{
+            //check if card has already been touched
+            if(parseInt(e.target.id) === animeData.id){
+                if(animeData.toggle === true){
+                    setScore(0)
+                    alert('over')
+                }
+            }
+            //marks the card as touched
             if(parseInt(e.target.id) === animeData.id){
                 animeData.toggle = true
                 // console.log(animeData)
                 setScore((prevScore)=>{
                     return prevScore + 1
                 })
-            }  else{
-                gameStatus(e)
             }
-            // console.log(animeData)
-
         })
     }
-
-    //check win or lose
-    const gameStatus = (e) => {
-        animeDatas.find((animeData)=>{
-            if(animeData.toggle === true){
-                animeData.toggle = false
-                if (animeData.toggle === false){
-                    console.log('ov')
-                }
-            } 
-        })
-    }
-
-
 
     //card finder/maker
     const showAnime = animeDatas.map((animeData)=>{
@@ -58,7 +48,6 @@ export default function Content() {
                 <div className="cards" onClick={checkCard} id={animeData.id}>
                     <img src={animeData.img} alt="anime pic"/>
                     <p>{animeData.name}</p>
-                    {/* <p>{animeData.toggle}</p> */}
                 </div>
             </div>
         )
@@ -74,21 +63,3 @@ export default function Content() {
         </div>
     )
 }
-
-
-// else if (animeData.toggle === false){
-//     animeData.toggle = false
-//     console.log('over')
-//     setScore(0)
-// }
-
-
-    // check win or lose
-    // const gameStatus = (e) => {
-    //     animeDatas.find((animeData)=>{
-    //         if(parseInt(e.target.id) === animeData.id){
-    //             animeData.toggle = false
-    //             alert('over')
-    //         }
-    //     })
-    // }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className='footer-container'>
+            <h3>.</h3>
+        </footer>
+    )
+}
