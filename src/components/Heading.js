9f61055ff2e102bca7d7d47ed4a5c82692f0ccd1
ji <jiyongprk@gmail.com>
2022-04-09 12:21:49 -0400

    add text animations for better looks!

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -3,7 +3,25 @@ import React from 'react';
 export default function Heading() {
     return (
         <header className="title-container">
-            <h1>Memory Card </h1>
+            <div className="text-container">
+                <span>M</span>
+                <span>E</span>
+                <span>M</span>
+                <span>E</span>
+                <span>O</span>
+                <span>R</span>
+                <span>Y</span>
+                <br></br>
+                <span>C</span>
+                <span>A</span>
+                <span>R</span>
+                <span>D</span>
+                <br></br>
+                <span>G</span>
+                <span>A</span>
+                <span>M</span>
+                <span>E</span>
+            </div>
         </header>
     )
 }
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function ScoreBoard(props) {
     return (
-        <div className='score-container'>
-            <h2 className='score'>Score: {props.score}</h2>
-            <h2 className='high-score'> High Score: {props.highScore}</h2>
+        <div className='score-container text-container'>
+            <span Style="font-size: 1.5rem" className='score'>Score: <span className="score-number">{props.score}</span></span>
+            <span Style="font-size: 1.5rem" className='high-score'> High Score: <span className='hscore-number'>{props.highScore}</span></span>
         </div>
     )
 }
